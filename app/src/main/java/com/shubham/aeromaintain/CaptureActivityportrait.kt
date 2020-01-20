6c2057a9cc04631f4a009adefa9ce805290b49fd
Mr. Shubham Kumar <# Created by https://www.gitignore.io/api/androidstudio # Edit at https://www.gitignore.io/?templates=androidstudio  ### AndroidStudio ### # Covers files to be ignored for android development using Android Studio.  # Built application files *.apk *.ap_  # Files for the ART/Dalvik VM *.dex  # Java class files *.class  # Generated files bin/ gen/ out/  # Gradle files .gradle .gradle/ build/  # Signing files .signing/  # Local configuration file (sdk path, etc) local.properties  # Proguard folder generated by Eclipse proguard/  # Log Files *.log  # Android Studio /*/build/ /*/local.properties /*/out /*/*/build /*/*/production captures/ .navigation/ *.ipr *~ *.swp  # Android Patch gen-external-apklibs  # External native build folder generated in Android Studio 2.2 and later .externalNativeBuild  # NDK obj/  # IntelliJ IDEA *.iml *.iws /out/  # User-specific configurations .idea/caches/ .idea/libraries/ .idea/shelf/ .idea/workspace.xml .idea/tasks.xml .idea/.name .idea/compiler.xml .idea/copyright/profiles_settings.xml .idea/encodings.xml .idea/misc.xml .idea/modules.xml .idea/scopes/scope_settings.xml .idea/dictionaries .idea/vcs.xml .idea/jsLibraryMappings.xml .idea/datasources.xml .idea/dataSources.ids .idea/sqlDataSources.xml .idea/dynamic.xml .idea/uiDesigner.xml .idea/assetWizardSettings.xml  # OS-specific files .DS_Store .DS_Store? ._* .Spotlight-V100 .Trashes ehthumbs.db Thumbs.db  # Legacy Eclipse project files .classpath .project .cproject .settings/  # Mobile Tools for Java (J2ME) .mtj.tmp/  # Package Files # *.war *.ear  # virtual machine crash logs (Reference: http://www.java.com/en/download/help/error_hotspot.xml) hs_err_pid*  ## Plugin-specific files:  # mpeltonen/sbt-idea plugin .idea_modules/  # JIRA plugin atlassian-ide-plugin.xml  # Mongo Explorer plugin .idea/mongoSettings.xml  # Crashlytics plugin (for Android Studio and IntelliJ) com_crashlytics_export_strings.xml crashlytics.properties crashlytics-build.properties fabric.properties  ### AndroidStudio Patch ###  !/gradle/wrapper/gradle-wrapper.jar  # End of https://www.gitignore.io/api/androidstudio>
2020-01-21 04:32:59 +0530

    qr added

--- app/src/main/java/com/shubham/aeromaintain/CaptureActivityportrait.kt
+++ app/src/main/java/com/shubham/aeromaintain/CaptureActivityportrait.kt
@@ -1,13 +1,6 @@
 package com.shubham.aeromaintain
 
 //import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.journeyapps.barcodescanner.CaptureActivity
 
-class CaptureActivityportrait : CaptureActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_capture_activityportrait)
-    }
-}
+class CaptureActivityportrait : CaptureActivity()
